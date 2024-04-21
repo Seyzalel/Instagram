884ba50c742ccaeacddaf0680c467cf680bdc792
Seyzalel Menabel <127889263+Seyzalel@users.noreply.github.com>
2024-04-21 12:30:25 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,42 +1,16 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-import telebot
-import os
+import asyncio
+from pyppeteer import launch
 
-TOKEN = '7091104502:AAHAkisdLFjjsuLEGoYbnuMkG86U4Tvai_g'
-bot = telebot.TeleBot(TOKEN)
-
-def take_screenshot():
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--window-size=1920x1080")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("accept-language=pt-BR,pt;q=0.9")
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
-    chrome_options.add_argument(f"user-agent={user_agent}")
-    
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-    
+async def main():
+    browser = await launch(headless=True, args=['--no-sandbox', '--disable-setuid-sandbox', '--disable-infobars', '--window-size=1920,1080'])
+    page = await browser.newPage()
+    await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
+    await page.goto('https://stresse.net/login')
     try:
-        driver.get("https://instagram.com/")
-        screenshot_path = "instagram_screenshot.png"
-        driver.save_screenshot(screenshot_path)
-    except Exception as e:
-        print(f"Erro: {e}")
-    finally:
-        driver.quit()
-    return screenshot_path
+        await page.waitForSelector('input[type="text"][required="required"][placeholder="Username"].form-control', timeout=30000)
+        print("Acessou a página com sucesso.")
+    except:
+        print("Elemento não encontrado.")
+    await browser.close()
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    screenshot_path = take_screenshot()
-    with open(screenshot_path, "rb") as photo:
-        bot.send_photo(message.chat.id, photo)
-    os.remove(screenshot_path)
-
-bot.polling()
+asyncio.get_event_loop().run_until_complete(main())
